6dcc5c1324d8d6bf571a43bcb643c0d4398023d9
Vince Yip <vyip12@mybcit.ca>
2013-11-26 13:34:26 -0800

    Added Bunch of files

---
+++ Gambia/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
+}
+
+
+<!--  Gabriel was born in Richmond, BC and has spent most his life in the Lower
+                Mainland. He loves playing sports, video games, watching movies and of course
+                computers! Therefore he's currently in his first term at BCIT studying
+                computer programming. He hopes to learn lots in software programming. -->
